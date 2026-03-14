63b51c313246b3218bb2d5dd21e046d8bba4f0a2
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-03-14 15:46:41 +0100

    chore(build): Add --verbose flag to provide more output

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,17 +1,37 @@
-import { exec } from "node:child_process"
+import { spawn } from "node:child_process"
 import { resolve } from "node:path"
 
 const root = resolve(import.meta.dirname, "..")
 
+const printHeadline = text => {
+  const divider = "-".repeat(text.length + 1)
+  process.stdout.write("\n")
+  process.stdout.write(`${divider}\n`)
+  process.stdout.write(` ${text}\n`)
+  process.stdout.write(`${divider}\n`)
+  process.stdout.write("\n")
+}
+
 const run = cmd =>
   new Promise((resolve, reject) => {
-    exec(cmd, { cwd: root, encoding: "utf-8" }, (error, stdout, stderr) => {
-      if (!error) return resolve(stdout)
-      return reject(stderr)
+    const child = spawn(`pnpm exec ${cmd}`, {
+      cwd: root,
+      shell: true,
+      stdio: "inherit",
+    })
+
+    child.on("error", reject)
+
+    child.on("close", code => {
+      if (code !== 0) process.exit(1)
+      resolve()
     })
   })
 
+printHeadline("🛠️ Bundling javascript")
 await run("vite build")
+
+printHeadline("🛠️ Transpiling types")
 await run(
   "tsc -p ./tsconfig.lib.json --outDir ./dist --noEmit false --declaration --emitDeclarationOnly"
 )
--- scripts/cli.mjs
+++ scripts/cli.mjs
@@ -2,27 +2,52 @@
 
 import { spawnSync } from "node:child_process"
 
-const commands = {
-  build: {
+const commands = [
+  {
+    name: "build",
     description: "Transpile the library to usable code",
     script: "./build.mjs",
   },
-}
+]
+
+const flags = [
+  { name: "help", description: "Display the help page" },
+  { name: "verbose", description: "Print more information in the terminal" },
+]
 
 const args = process.argv.slice(2)
-const command = commands[args[0] ?? ""]
+const command = args.reduce((result, arg) => {
+  const cmd = commands.find(({ name }) => name === arg)
+  if (!result.name && cmd) {
+    return { ...cmd, ...result }
+  }
 
-if (!command) {
+  const flag = flags.find(({ name }) => `--${name}` == arg)
+  if (flag) {
+    return { [flag.name]: true, ...result }
+  }
+
+  console.error(`Argument ${arg} is invalid.\n`)
+  return { help: true }
+}, {})
+
+if (!command.name || command.help) {
   console.info("Usage: boring-blocks <command>")
   console.info("\nAvailable commands:")
-  Object.entries(commands).forEach(([cmd, { description }]) => {
-    console.info(`  ${cmd}: ${description}`)
+  commands.forEach(({ name, description }) => {
+    console.info(`  ${name}: ${description}`)
+  })
+  console.info("\nAvailable flags:")
+  flags.forEach(({ name, description }) => {
+    console.info(`  --${name}: ${description}`)
   })
   process.exit(1)
 }
 
+console.info(`ℹ️ Running boring-blocks ${command.name}...`)
+
 spawnSync(`node ${command.script}`, {
   cwd: import.meta.dirname,
-  stdio: "inherit",
+  stdio: command.verbose ? "inherit" : "pipe",
   shell: true,
 })
