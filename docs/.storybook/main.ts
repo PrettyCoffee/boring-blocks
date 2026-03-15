42eba1baa5565414d5a83a734841d1bf64f6edde
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-03-15 02:12:25 +0100

    docs(storybook): Fix fonts in sidebar

--- docs/.storybook/main.ts
+++ docs/.storybook/main.ts
@@ -1,5 +1,29 @@
 import type { StorybookConfig } from "@storybook/react-vite"
 
+const loadFonts = `
+<link
+  rel="preload"
+  href="./fonts/FiraCode-Variable.ttf"
+  as="font"
+  type="font/ttf"
+  crossorigin="anonymous"
+/>
+<link
+  rel="preload"
+  href="./fonts/Quicksand-Variable.ttf"
+  as="font"
+  type="font/ttf"
+  crossorigin="anonymous"
+/>
+<link rel="stylesheet" href="./fonts/fonts.css" />
+
+<style>
+  .sbdocs p {
+    font-size: 1rem;
+  }
+</style>
+`
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)", "../src/**/*.mdx"],
   addons: [
@@ -11,5 +35,7 @@ const config: StorybookConfig = {
     options: { strictMode: true },
   },
   core: { disableTelemetry: true },
+  managerHead: head => `${head} ${loadFonts}`,
+  previewHead: head => `${head} ${loadFonts}`,
 }
 export default config
