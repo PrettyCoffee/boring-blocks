c12966f957edbc36f15fd02bbe2207dd734417af
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-03-16 00:12:42 +0100

    docs: Update storybook logo to animated sloth

--- docs/.storybook/addons/dark-mode/DarkModeToggle.tsx
+++ docs/.storybook/addons/dark-mode/DarkModeToggle.tsx
@@ -1,17 +1,64 @@
 // @ts-ignore -- We need to import React for JSX support in Storybook internals
 import React from "react"
 
-import { MoonIcon, SunIcon } from "@storybook/icons"
-import { IconButton } from "storybook/internal/components"
+import { css, styled } from "storybook/theming"
 
 import { darkMode } from "./darkMode"
 import { useAtom } from "../../../src/lib/yaasl"
+
+const ToggleButton = styled.button(
+  ({ theme: { barHoverColor, appBorderRadius } }) => css`
+    width: 28px;
+    height: 28px;
+    padding: 1px;
+    display: inline-grid;
+    place-content: center;
+    border: none;
+    background: transparent;
+    cursor: pointer;
+
+    position: relative;
+
+    ::after {
+      content: "";
+      display: inline-block;
+      position: absolute;
+      inset: 0;
+      border-radius: ${appBorderRadius}px;
+      --opacity: 0%;
+      background: color-mix(
+        in srgb,
+        ${barHoverColor} var(--opacity),
+        transparent
+      );
+    }
+    :hover::after {
+      --opacity: 15%;
+    }
+    :active::after {
+      --opacity: 25%;
+    }
+
+    > img {
+      z-index: 1;
+      height: 100%;
+      width: 100%;
+      opacity: 0.5;
+    }
+    :hover > img {
+      opacity: 0.9;
+    }
+    :active > img {
+      opacity: 1;
+    }
+  `
+)
 
 export const DarkModeToggle = () => {
   const isDarkMode = useAtom(darkMode)
 
   return (
-    <IconButton
+    <ToggleButton
       key="dark-mode-toggle"
       title={
         isDarkMode ? "Change theme to light mode" : "Change theme to dark mode"
@@ -19,10 +66,10 @@ export const DarkModeToggle = () => {
       onClick={() => darkMode.set(!isDarkMode)}
     >
       {isDarkMode ? (
-        <SunIcon aria-hidden="true" />
+        <img src="/moon.png" alt="dark mode" />
       ) : (
-        <MoonIcon aria-hidden="true" />
+        <img src="/sun.png" alt="light mode" />
       )}
-    </IconButton>
+    </ToggleButton>
   )
 }
