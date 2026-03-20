9fc57c6269db398481cba6c5e186c12e8704f4bb
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-03-20 01:20:51 +0100

    chore(eslint): Restrict lucide-react imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,56 @@ import prettyCozy from "@pretty-cozy/eslint-config"
 import { defineConfig, globalIgnores } from "eslint/config"
 import storybook from "eslint-plugin-storybook"
 
+const storybookConfig = defineConfig({
+  basePath: "./docs",
+
+  rules: {
+    "import-x/no-extraneous-dependencies": "off",
+  },
+
+  extends: [
+    // eslint-disable-next-line import-x/no-named-as-default-member
+    storybook.configs["flat/recommended"],
+
+    {
+      name: "local-rules/check-file-naming",
+      files: [".storybook/**"],
+      rules: {
+        "check-file/folder-naming-convention": "off",
+        "storybook/no-uninstalled-addons": "off",
+      },
+    },
+
+    {
+      name: "local-rules/lib-imports",
+      files: ["src/**"],
+      ignores: ["src/lib/**"],
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@yaasl/*"],
+                importNamePattern: "^",
+                message: "Import from lib/yaasl instead.",
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+})
+
 export default defineConfig(
   prettyCozy.baseTs,
   prettyCozy.react,
   prettyCozy.tailwind({ entryPoint: "docs/src/index.css" }),
   prettyCozy.vitest,
   globalIgnores(["dist", "node_modules", "!./docs/.storybook"]),
+
+  storybookConfig,
 
   {
     rules: {
@@ -20,51 +64,21 @@ export default defineConfig(
   },
 
   {
-    files: ["./*", "docs/**"],
-    rules: {
-      "import-x/no-extraneous-dependencies": "off",
-    },
-  },
-
-  {
     name: "local-rules/lib-imports",
-    files: ["docs/src/**"],
-    ignores: ["docs/src/lib/**"],
+    files: ["src/**"],
+    ignores: ["src/components/icons/**"],
     rules: {
       "no-restricted-imports": [
         "error",
         {
           patterns: [
             {
-              group: ["@yaasl/*"],
+              group: ["lucide-react"],
               importNamePattern: "^",
-              message: "Import from lib/yaasl instead.",
+              message: "Import from /components/icons instead.",
             },
           ],
         },
-      ],
-    },
-  },
-
-  {
-    basePath: "./docs",
-    extends: [
-      // eslint-disable-next-line import-x/no-named-as-default-member
-      storybook.configs["flat/recommended"],
-      {
-        name: "local-rules/check-file-naming",
-        files: [".storybook/**"],
-        rules: {
-          "check-file/folder-naming-convention": "off",
-          "storybook/no-uninstalled-addons": "off",
-        },
-      },
-    ],
-    rules: {
-      "check-file/filename-naming-convention": [
-        "error",
-        { "*/**": "KEBAB_CASE" },
-        { ignoreMiddleExtensions: true },
       ],
     },
   },
