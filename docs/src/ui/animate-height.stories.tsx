d1e56e18d0a4f73df6a2ed23278e6108498b2664
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-04-11 22:39:04 +0200

    docs(AnimateHeight): Add more options for testing

--- docs/src/ui/animate-height.stories.tsx
+++ docs/src/ui/animate-height.stories.tsx
@@ -12,7 +12,7 @@ const meta = {
   component: AnimateHeight,
   argTypes: {
     duration: argType.number(),
-    height: argType.enum("radio", ["auto", 100, 200, 300]),
+    height: argType.enum("radio", ["auto", 0, 100, 200, 300]),
     onTransitionStart: argType.callback(),
     onTransitionEnd: argType.callback(),
   },
@@ -26,21 +26,97 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+const demoHeights: Record<
+  string,
+  { defautlValue?: number; values?: number[] }
+> = {
+  auto: {},
+  number: { defautlValue: 150, values: [0, 100, 150, 200] },
+  px: { defautlValue: 150, values: [0, 100, 150, 200] },
+  rem: { defautlValue: 10, values: [0, 5, 10, 15] },
+  vh: { defautlValue: 25, values: [0, 10, 25, 50] },
+  "%": { defautlValue: 50, values: [0, 25, 50, 75, 100] },
+}
 export const Default: Story = {
+  argTypes: { height: argType.disabled() },
   render: args => {
-    const [contentHeight, setContentHeight] = useState(100)
+    const [selectedType, setSelectedType] = useState("number")
+    const [heightValue, setHeightValue] = useState(100)
+    const [contentHeight, setContentHeight] = useState<number | string>("100%")
+
+    const heightOptions = demoHeights[selectedType]?.values ?? []
+
+    const height =
+      selectedType === "auto"
+        ? "auto"
+        : selectedType === "number"
+          ? heightValue
+          : heightValue + selectedType
+
     return (
       <>
         <div className={cn(hstack({ gap: 2, align: "center" }), "mb-2")}>
-          Content Height:
-          <Button onClick={() => setContentHeight(100)}>100px</Button>
-          <Button onClick={() => setContentHeight(150)}>150px</Button>
-          <Button onClick={() => setContentHeight(200)}>200px</Button>
+          Height type:
+          {Object.keys(demoHeights).map(type => (
+            <Button
+              key={type}
+              onClick={() => {
+                setSelectedType(type)
+                setHeightValue(demoHeights[type]?.defautlValue ?? 0)
+                setContentHeight("100%")
+              }}
+              active={selectedType === type}
+            >
+              {type}
+            </Button>
+          ))}
         </div>
 
-        <AnimateHeight {...args} className="w-96 p-2 outline outline-highlight">
-          <Placeholder style={{ height: contentHeight + "px" }} />
-        </AnimateHeight>
+        {selectedType === "auto" ? (
+          <div className={cn(hstack({ gap: 2, align: "center" }), "mb-2")}>
+            Content Height:
+            {[0, 100, 150, 200, "100%"].map(height => (
+              <Button
+                key={height}
+                onClick={() => setContentHeight(height)}
+                active={contentHeight === height}
+              >
+                {height}
+              </Button>
+            ))}
+          </div>
+        ) : (
+          <div className={cn(hstack({ gap: 2, align: "center" }), "mb-2")}>
+            Height value:
+            {heightOptions.map(height => (
+              <Button
+                key={height}
+                onClick={() => setHeightValue(height)}
+                active={heightValue === height}
+              >
+                {height}
+                {selectedType !== "number" && selectedType}
+              </Button>
+            ))}
+          </div>
+        )}
+
+        <div className="mb-2 font-mono text-sm">{`height={${typeof height === "number" ? height : `"${height}"`}}`}</div>
+
+        <div
+          className={cn(
+            selectedType === "%" &&
+              "h-48 w-max outline outline-offset-1 outline-stroke-gentle"
+          )}
+        >
+          <AnimateHeight
+            {...args}
+            height={height}
+            className="w-96 p-2 outline outline-highlight"
+          >
+            <Placeholder style={{ height: contentHeight }} />
+          </AnimateHeight>
+        </div>
       </>
     )
   },
